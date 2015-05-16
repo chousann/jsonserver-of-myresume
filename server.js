87cde89b3ad0bb1272bc4311e08dfbb486ae7476
Espen Halvorsen <espenh@unipluss.no>
2015-05-16 15:30:42 +0200

    Change from in-memory to file

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var db = require('./generate').run();
 
 var server = jsonServer.create();        // Express server 
 server.use(jsonServer.defaults);         // Default middlewares (logger, public, cors) 
-server.use(jsonServer.router(db));       // Express router 
+//server.use(jsonServer.router(db));       // Express router 
+server.use(jsonServer.router('db.json'));       // Express router 
 
 var port = process.env.PORT || 3000;
 

3ccf8a8c49f81230b3ede1ad44da9861eb4ed6c3
chousann <1769862201@qq.com>
2019-04-27 17:14:34 +0800

    jsonserver of myresume

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var jsonServer = require('json-server');
 
-var db = require('./generate').run();
+//var db = require('./generate').run();
 
 var server = jsonServer.create();        // Express server 
 server.use(jsonServer.defaults);         // Default middlewares (logger, public, cors) 
 //server.use(jsonServer.router(db));       // Express router 
-server.use(jsonServer.router('db.json'));       // Express router 
-
+// server.use(jsonServer.router('db.json'));       // Express router 
+server.use(jsonServer.router('info.json'));       // Express router 
 var port = process.env.PORT || 3000;
 
 server.listen(port);
